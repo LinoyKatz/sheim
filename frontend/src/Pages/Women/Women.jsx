35d25aa1331f9068e6eed8301c2e4792c24ec5e0
Linoy Katz <katzlinoy1269@gmail.com>
2023-06-30 14:43:40 +0300

    env

--- frontend/src/Pages/Women/Women.jsx
+++ frontend/src/Pages/Women/Women.jsx
@@ -10,24 +10,16 @@ import { BiDownvote } from "react-icons/bi";
 import ProductsHeadline from "../../Components/Products-headline/ProductsHeadline";
 const Women = () => {
   // // Get the button:
-  let mybutton = document.getElementById("myBtn");
+  // let mybutton = document.getElementById("myBtn");
 
   // // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function () {
-    scrollFunction();
+  const backToTop = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
   };
-
-  function scrollFunction() {
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  }
-
   // // When the user clicks on the button, scroll to the top of the document
   // function topFunction() {
   //   document.body.scrollTop = 0; // For Safari
